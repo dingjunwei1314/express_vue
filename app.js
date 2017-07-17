3f499efffdd96df1592e7d38a6e5b3cbd4e0a844
dingjunwei1314 <17839359913@163.com>
2017-07-17 17:42:14 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -10,9 +10,20 @@ var express = require('express'),
     app = express();
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    upload=require('./mids/multer');/*需要通过multer中间件处理上传的文件，express默认不会对文件进行处理*/
+    upload=require('./mids/multer'),/*需要通过multer中间件处理上传的文件，express默认不会对文件进行处理*/
+    my_soket=require('./socket/index');
+//设置请求头跨域
+app.all('*',function(req, res, next) {
 
-    
+    res.header("Access-Control-Allow-Origin", "http://localhost:8088");//设置可以跨域的前端域名
+    res.header("Access-Control-Allow-Credentials", true);//设置可以携带cookie
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");//设置请求方法
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+
+});
 
 /*
 如果是用ejs模板全后端不分离的话需要进行如下设置
@@ -21,22 +32,19 @@ app.set('view engine', 'ejs');
 */
 
 app.use(logger('dev'));
+
 /*需要通过bodyParser中间件处理json数据,express默认不处理*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-app.use(express.static(path.join(__dirname, 'vue-project')));
-app.use(express.static(path.join(__dirname, 'public')));
-
 //将vue项目设置为静态文件实现前后端分离
+// app.use(express.static(path.join(__dirname, 'vue-project')));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'uploads')));
 
 //这里传入了一个密钥加session id
 app.use(cookieParser('dingjunwei'));
-
-
-
 
 //session中间件设置服务端session
 app.use(session({
@@ -53,9 +61,10 @@ app.use(session({
     })
 }))
 
-
 //路由
 routes(app);
+//socket
+my_soket()
 server.listen(3000);
 
 
